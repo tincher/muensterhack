142b0512088aa786054b57d5e1b0a31287c40e30
tincher <joel.ewig@gmail.com>
2026-09-25 15:01:29 +0200

    feat: map with variable marker

--- src/server.py
+++ src/server.py
@@ -6,8 +6,14 @@ app = Flask(__name__)
 env = jinja2.Environment(loader=jinja2.FileSystemLoader("./assets/maps/"))
 
 
+marker_template = (
+    "L.marker({{lon: {lon}, lat: {lat}}}).bindPopup('{popup_text}').addTo(map);"
+)
+
+
 @app.route("/")
 def index():
+    print(marker_template.format(lon=7.641, lat=51.952, popup_text="items"))
     return env.get_template("base_map.jinja2").render(
-        marker_lon=7.641, marker_lat=51.952
+        markers_js=marker_template.format(lon=7.641, lat=51.952, popup_text="items")
     )
