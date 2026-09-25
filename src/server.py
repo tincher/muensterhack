c8ebdf4b823d75c1149c826330f9f693c7f4fd24
tincher <joel.ewig@gmail.com>
2026-09-25 17:03:37 +0200

    feat: add website_builder

--- src/server.py
+++ src/server.py
@@ -3,19 +3,18 @@ from flask import Flask
 
 from src.coordinates import Coordinates
 from src.routing import get_route
-from src.template_handler import TemplateHandler
+from src.website_builder import WebsiteBuilder
 
 load_dotenv()
 app = Flask(__name__)
-template_handler = TemplateHandler()
 
 
 @app.route("/")
 def index():
     route = get_route(Coordinates(lon=7.641, lat=51.952), Coordinates(lon=7.626, lat=51.962))
-    return template_handler.get_route_example(route)
+    return WebsiteBuilder().get_route_example(route).render()
 
 
 @app.route("/example_marker")
 def example_marker():
-    return template_handler.get_example()
+    return WebsiteBuilder().get_example().render()
---
+++ src/website_builder.py
@@ -0,0 +1,60 @@
+import jinja2
+
+
+class WebsiteConfig:
+    code: str = """var map = L.map('map').setView({lon: 7.641, lat: 51.952}, 13);
+            // add the OpenStreetMap tiles
+            L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+                maxZoom: 19,
+                attribution: '&copy; <a href="https://openstreetmap.org/copyright">OpenStreetMap contributors</a>'
+            }).addTo(map);
+
+            // show the scale bar on the lower left corner
+            L.control.scale({imperial: true, metric: true}).addTo(map);"""
+
+    def append(self, new_code):
+        self.code = self.code + new_code
+
+
+class WebsiteBuilder:
+    def __init__(self):
+        self.env = jinja2.Environment(loader=jinja2.FileSystemLoader("./assets/maps/"))
+        self.marker_template = "L.marker({{lon: {lon}, lat: {lat}}}).bindPopup('{popup_text}').addTo(map);"
+        # self.geojson_template = r"""L.geoJSON({{data}}, {
+        #                     style: function (feature) {
+        #                         return {color: feature.properties.color};
+        #                     }
+        #                 }).bindPopup(function (layer) {
+        #                     return layer.feature.properties.description;
+        #                 }).addTo(map);
+        #                 """
+        self.geojson_template = r"""L.geoJSON({{data}}).addTo(map);"""
+        self.website_config = WebsiteConfig()
+
+    def render(self):
+        return self.env.get_template("base_map.jinja2").render(leaflet_code=self.website_config.code)
+
+    def add_marker(self, lat: float, lon: float, popup_text: str):
+        self.website_config.append(self.marker_template.format(lon=lon, lat=lat, popup_text=popup_text))
+        return self
+
+    def add_route(self, data):
+        self.website_config.append(self.env.from_string(self.geojson_template).render(data=data))
+        return self
+
+    def get_example(self):
+        self.add_marker(lon=7.641, lat=51.952, popup_text="Items")
+        return self
+
+    def get_route_example(self, data):
+        self.add_route(data)
+        return self
+
+    # def get_example(self, template="base_map.jinja2"):
+    #     return self.env.get_template(template).render(
+    #         leaflet_code=self.base + self.marker_template.format(lon=7.641, lat=51.952, popup_text="Items")
+    #     )
+
+    # def get_route_example(self, data, template="base_map.jinja2"):
+    #     geojson_rendered = self.env.from_string(self.geojson_template).render(data=data)
+    #     return self.env.get_template(template).render(leaflet_code=self.base + geojson_rendered)
