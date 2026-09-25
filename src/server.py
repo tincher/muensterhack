7461a36558297150a65bc454f4770b5b5f683c3a
tincher <joel.ewig@gmail.com>
2026-09-25 21:44:42 +0200

    feat: add car routing

--- src/server.py
+++ src/server.py
@@ -2,9 +2,9 @@ from dotenv import load_dotenv
 from flask import Flask
 
 from src.coordinates import Coordinates
-from src.routing import get_shade_wheelchair_route
-from src.website_builder import WebsiteBuilder
 from src.db_handler import DatabaseHandler
+from src.routing import get_complete_route
+from src.website_builder import WebsiteBuilder
 
 load_dotenv()
 app = Flask(__name__)
@@ -12,7 +12,8 @@ app = Flask(__name__)
 
 @app.route("/example_route")
 def index():
-    route = get_shade_wheelchair_route(Coordinates(lon=7.641, lat=51.952), Coordinates(lon=7.626, lat=51.962))
+    # 51.962713, 7.625652
+    route = get_complete_route(Coordinates(lon=7.641, lat=51.952), Coordinates(lon=7.625652, lat=51.962713), [])
     return WebsiteBuilder().get_route_example(route).render()
 
 
