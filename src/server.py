1befb86107b7db12bec4a184a90f72919e769c10
tincher <joel.ewig@gmail.com>
2026-09-25 15:33:07 +0200

    feat: add setup script

--- src/server.py
+++ src/server.py
@@ -1,6 +1,9 @@
+from dotenv import load_dotenv
 from flask import Flask
+
 from src.template_handler import TemplateHandler
 
+load_dotenv()
 app = Flask(__name__)
 template_handler = TemplateHandler()
 
--- start_server.sh
+++
@@ -1,2 +0,0 @@
-flask --app src.server --debug run
-
