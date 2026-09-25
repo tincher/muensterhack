feca2385cea7e70cdc382814e7ef22643d30e7c9
tincher <joel.ewig@gmail.com>
2026-09-25 14:52:19 +0200

    feat: map with marker

--- src/server.py
+++ src/server.py
@@ -1,8 +1,13 @@
 from flask import Flask
+import jinja2
+
 
 app = Flask(__name__)
+env = jinja2.Environment(loader=jinja2.FileSystemLoader("./assets/maps/"))
 
 
 @app.route("/")
 def index():
-    return "Hello World!"
+    return env.get_template("base_map.jinja2").render(
+        marker_lon=7.641, marker_lat=51.952
+    )
